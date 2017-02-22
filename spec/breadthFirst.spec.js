11011d98de4bbfa5218420bcf3f960a01786d459
Lee Morris <leebenmorris@gmail.com>
2017-02-22 20:29:33 +0000

    basic sprint complete, solutions included

--- spec/breadthFirst.spec.js
+++ spec/breadthFirst.spec.js
@@ -1,8 +1,15 @@
 const expect = require('chai').expect;
 const breadthFirstSearch = require('../lib/breadthFirstSearch');
+const Graph = require('../lib/graphs')
+const testGraph = new Graph()
+//console.log(testGraph)
 
 describe('breadthFirstSearch', function () {
-    it('is a function', function () {
-        expect(breadthFirstSearch).to.be.a('function');
-    });
+  it('is a function', function () {
+    expect(breadthFirstSearch).to.be.a('function');
+  });
+  it('random test', function () {
+    let actual = breadthFirstSearch(testGraph, 'A', 'F')
+    expect(actual).to.equal(true);
+  });
 });
